a4e021fb45a82a26a8f65000d2be3a9b4e5fc8e1
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-06-11 13:41:49 -0500

    Terminando el diseño del formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,5 +7,77 @@
       Agrega Pacientes y
       <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <!-- Nombre de la mascota -->
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold">
+          Nombre Mascota
+        </label>
+        <input
+          type="text"
+          id="mascota"
+          placeholder="Nombre de la mascota"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <!-- Propietario de la mascota -->
+      <div class="mb-5">
+        <label
+          for="propietario"
+          class="block text-gray-700 uppercase font-bold"
+        >
+          Nombre Propietario
+        </label>
+        <input
+          type="text"
+          id="propietario"
+          placeholder="Nombre del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <!-- Email del propietario -->
+      <div class="mb-5">
+        <label for="email" class="block text-gray-700 uppercase font-bold">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <!-- Fecha de alta de la mascota -->
+      <div class="mb-5">
+        <label for="alta" class="block text-gray-700 uppercase font-bold">
+          Alta
+        </label>
+        <input
+          type="date"
+          id="alta"
+          class="border-2 w-full p-2 mt-2 rounded-md"
+        />
+      </div>
+      <!-- Síntomas dela mascota -->
+      <div class="mb-5">
+        <label for="sintomas" class="block text-gray-700 uppercase font-bold">
+          Sintomas
+        </label>
+        <textarea
+          name="sintomas"
+          class="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
+          id="sintomas"
+          cols="30"
+          rows="5"
+          placeholder="Describe los síntomas"
+        ></textarea>
+      </div>
+      <!-- Botón  -->
+      <input
+        type="submit"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Registrar Paciente"
+      />
+    </form>
   </div>
 </template>
