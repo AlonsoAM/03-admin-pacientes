5831978877eca1942eb154de0d1ae1d6db3ebcd5
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-06-11 15:04:38 -0500

    Agregando dinamismo a las alertas

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  alerta: {
+    type: Object,
+    required: true,
+  },
+});
+const isError = computed(() => props.alerta.tipo === "error");
+</script>
 <template>
-  <div>Alerta</div>
+  <div
+    :class="isError ? 'bg-red-500 ' : 'bg-green-500 '"
+    class="text-white text-center uppercase font-bold mb-4 rounded-md py-3 px-5"
+  >
+    {{ alerta.mensaje }}
+  </div>
 </template>
